360bc0073c906a5a75f0ac0499741dca0ec1597e
JonasB91 <jonas.backas@hotmail.com>
2023-09-05 10:30:09 +0200

    pushar senaste ändringar försökt fixa nnavbar

--- Javascript/navbar.js
+++ Javascript/navbar.js
@@ -6,12 +6,3 @@ fetch('nav.html')
     newelem.innerHTML = text;
     oldelem.parentNode.replaceChild(newelem,oldelem);
 })
-
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
